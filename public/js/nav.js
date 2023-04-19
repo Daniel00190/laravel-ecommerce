a1ab4f057299385390942c5380b8bc4f8c590697
Daniel00190 <69172476+Daniel00190@users.noreply.github.com>
2023-04-19 19:43:11 -0400

    package.json update

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*****************************!*\
-  !*** ./resources/js/nav.js ***!
-  \*****************************/
-var navLinks = document.querySelector('.nav-links');
-var menuIcon = document.querySelector('.menu-icon');
-menuIcon.addEventListener('click', function () {
-  console.log("clicked");
-  if (navLinks.classList.contains('show')) {
-    navLinks.classList.remove('show');
-  } else {
-    navLinks.classList.add('show');
-  }
-});
-/******/ })()
-;
+(()=>{var e=document.querySelector(".nav-links");document.querySelector(".menu-icon").addEventListener("click",(function(){console.log("clicked"),e.classList.contains("show")?e.classList.remove("show"):e.classList.add("show")}))})();
